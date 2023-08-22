1614d130e2dbf05bf14fe6cba1a6569e5e82def2
Abdullah-Naifeh <abdna59@gmail.com>
2023-08-22 15:33:28 +0300

    modifed the navbar styling and replaced it to layout.tsx page

--- store/app/page.tsx
+++ store/app/page.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import TopHeader from '../components/topHeaderNav/topHeader'
 import Container from "@mui/material/Container";
 
 const theme = createTheme({
@@ -31,29 +30,9 @@ const theme = createTheme({
 
 export default function Home() {
   return (
-    <Container>
-      <TopHeader />
-      {/* <ThemeProvider theme={theme}>
-        <main>
-          <Button variant="contained">Primary</Button>
-
-          <Button variant="contained" color="secondary">
-            Secondary
-          </Button>
-          <Button variant="contained" color="warning">
-            warning
-          </Button>
-          <Button variant="contained" color="error">
-            error
-          </Button>
-          <Button variant="contained" color="success">
-            success
-          </Button>
-          <Button variant="contained" color="info">
-            info
-          </Button>
-        </main>
-      </ThemeProvider> */}
-    </Container>
+    <>
+      <Container>
+      </Container>
+    </>
   );
 }
