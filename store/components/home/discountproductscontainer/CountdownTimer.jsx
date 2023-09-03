135ad4f9002d9976e65644cfaff0201fc919de34
waelkamira <ramondm4@gmail.com>
2023-09-03 22:36:26 +0300

    done

--- store/components/home/discountproductscontainer/CountdownTimer.jsx
+++ store/components/home/discountproductscontainer/CountdownTimer.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import Countdown from "react-countdown";
-
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
+import styles from "./styles.module.css";
+const renderer = ({ days, hours, minutes, seconds }) => {
   return (
-    <div style={{ display: "flex", flexGrow: "1" }}>
-      <div>
-        <h1>{days}</h1>
-        <h1>يوم</h1>
+    <div
+      style={{
+        display: "flex",
+        flexGrow: "1",
+        justifyContent: "space-between",
+        textAlign: "center",
+      }}
+    >
+      <div className={styles.countdownDivs}>
+        <h1 className={styles.countdownNumber}>{days}</h1>
+        <h1 className={styles.countdownText}>يوم</h1>
       </div>
-      <div>
-        <h1>{hours}</h1>
-        <h1>ساعة</h1>
+      <div className={styles.countdownDivs}>
+        <h1 className={styles.countdownNumber}>{hours}</h1>
+        <h1 className={styles.countdownText}>ساعة</h1>
       </div>
-      <div>
-        <h1>{minutes}</h1>
-        <h1>دقيقة</h1>
+      <div className={styles.countdownDivs}>
+        <h1 className={styles.countdownNumber}>{minutes}</h1>
+        <h1 className={styles.countdownText}>دقيقة</h1>
       </div>
-      <div>
-        <h1>{seconds}</h1>
-        <h1>ثانية</h1>
+      <div className={styles.countdownDivs}>
+        <h1 className={styles.countdownNumber}>{seconds}</h1>
+        <h1 className={styles.countdownText}>ثانية</h1>
       </div>
     </div>
   );
 };
-
 export default function CountdownTimer() {
   return (
     <div>
-      <Countdown date="2023-10-01T01:02:03" renderer={renderer} />;
+      <Countdown date="2023-10-01T01:02:03" renderer={renderer} />
     </div>
   );
 }
--- store/components/home/discountproductscontainer/DiscountProductsContainer.jsx
+++ store/components/home/discountproductscontainer/DiscountProductsContainer.jsx
@@ -4,7 +4,13 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import Image from "next/image";
 import photo from "./photos/Image.png";
 import CountdownTimer from "./CountdownTimer";
+import HoverRating from "./Rating";
+import * as React from "react";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import Discount from "./photos/Discount.png";
 export default function DiscountProductsContainer() {
+  const [value, setValue] = React.useState(0);
   return (
     <div className={styles.main}>
       <div className={styles.buttonAndTextBox}>
@@ -20,27 +26,135 @@ export default function DiscountProductsContainer() {
           <Button
             variant="outlined"
             color="primary"
-            style={{ marginBottom: "0rem" }}
+            style={{ marginBottom: "0vw" }}
           >
             عرض الكل
             <NavigateBeforeIcon />
           </Button>
         </div>
       </div>
-      <div>
-        <div>
-          <Image
-            src={photo}
-            alt="/"
+      <div className={styles.discountDivs}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "2px solid #62d0b6",
+            borderRadius: "5px",
+            flexGrow: "1",
+            padding: "1rem",
+            paddingBottom: "0rem",
+          }}
+        >
+          <div
             style={{
-              top: "1vw",
-              left: "1vw",
-              height: "auto",
+              position: "relative",
+              border: "1px solid green",
             }}
-          />
+          >
+            <Image
+              src={photo}
+              alt="/"
+              style={{
+                height: "auto",
+                minWidth: "3vw",
+                border: "1px solid green",
+              }}
+            />
+            <div style={{ position: "absolute", top: ".3vw", left: ".3vw" }}>
+              <Image
+                src={Discount}
+                alt="/"
+                style={{
+                  position: "relative",
+                  top: "1vw",
+                  left: "1vw",
+                  height: "auto",
+                }}
+              />
+              <h6
+                style={{
+                  position: "absolute",
+                  top: "1.2vw",
+                  left: "1.2vw",
+                  fontSize: "clamp(0.875rem, 0vw + 0.875rem, 0.875rem)",
+                  color: "white",
+                }}
+              >
+                %25خصم
+              </h6>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: "1",
+              border: "1px solid red",
+              height: "100%",
+            }}
+          >
+            <h1 style={{ color: "#62d0b6" }}>ساعات</h1>
+            <h1
+              style={{
+                fontSize: "clamp(1.125rem, 0vw + 1.125rem, 1.125rem)",
+                fontWeight: "700",
+              }}
+            >
+              ساعة ذكية جديدة من سلسلة 8
+            </h1>
+            <h1 style={{ color: "#9d9d9d" }}>سوار رياضي اسود - عادي.</h1>
+            <div>
+              <HoverRating />
+            </div>
+            <div style={{ display: "flex" }}>
+              <h1
+                style={{ color: "red", fontWeight: "700", marginLeft: "1vw" }}
+              >
+                250.00 رس
+              </h1>
+              <h1 style={{ color: "#9d9d9d", textDecoration: "line-through" }}>
+                350.00 ر.س
+              </h1>
+            </div>
+            <div
+              style={{
+                paddingTop: "0.5vw",
+                paddingBottom: "0vw",
+                display: "flex",
+                flexDirection: "column",
+                // border: "1px solid green",
+                marginBottom: "0vw",
+              }}
+            >
+              <CountdownTimer />
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <Button
+                  className={styles.btn}
+                  variant="outlined"
+                  color="primary"
+                >
+                  <ShoppingCartOutlinedIcon
+                    className={styles.icon}
+                    style={{
+                      fontSize: "clamp(1rem, 0vw + 1rem, 1rem)",
+                      margin: ".65vw",
+                    }}
+                  />
+                  أضف للسلة
+                </Button>
+                <Button className={styles.btnIcon} variant="outlined">
+                  <FavoriteBorderOutlinedIcon className={styles.icon} />
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <CountdownTimer />
     </div>
   );
 }
